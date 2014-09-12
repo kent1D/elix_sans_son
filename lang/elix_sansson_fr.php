888fe1721f2b1b75a50371310fdf504945bb7154
kent1 <kent1@arscenic.info>
2014-09-13 00:03:00 +0200

    Lister les videos de définition avec son

--- lang/elix_sansson_fr.php
+++ lang/elix_sansson_fr.php
@@ -4,4 +4,5 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_menu_sansson' => 'Vérification des bandes son',
+	'titre_page_elix_sansson' => 'Vérification des définitions ayant une bande son',
 );
