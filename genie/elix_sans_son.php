a08a9f8425d5e9768641423a832ec1c8380ee02c
kent1 <kent1@arscenic.info>
2014-10-06 06:30:48 +0200

    Améliorations

--- genie/elix_sans_son.php
+++ genie/elix_sans_son.php
@@ -15,8 +15,12 @@
  */
 function genie_elix_sans_son($time){
 	$id_document = sql_getfetsel('documents.id_document','spip_documents AS `documents` INNER JOIN spip_documents_liens AS L3 ON ( L3.id_document = documents.id_document ) INNER JOIN spip_articles AS L4 ON ( L4.id_article = L3.id_objet AND L3.objet="article")',"(documents.statut = 'publie') AND (documents.taille > 0 OR documents.distant='oui') AND (L4.id_secteur = 94715) AND (documents.mode = 'document') AND (documents.media = 'video') AND (documents.hasaudio = 'oui')",'','','0,1');
-	$traiter = charger_fonction('elix_sansson_reencode','action');
-	$traiter($id_document);
+	if(!$id_document)
+		$id_document = sql_getfetsel('documents.id_document','spip_documents AS `documents` INNER JOIN spip_documents_liens AS L3 ON ( L3.id_document = documents.id_document ) INNER JOIN spip_signes AS L4 ON ( L4.id_signe = L3.id_objet AND L3.objet="signe")',"(documents.statut = 'publie') AND (documents.taille > 0 OR documents.distant='oui') AND (L4.id_signe > 0) AND (documents.mode = 'document') AND (documents.media = 'video') AND (documents.hasaudio = 'oui')",'','','0,1');
+	if(intval($id_document)){
+		$traiter = charger_fonction('elix_sansson_reencode','action');
+		$traiter($id_document);
+	}
 	return 1;
 }
 
